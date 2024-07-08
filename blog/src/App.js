20475f1a9fb52d74855e974a56622847f06564b6
jovid <sh.jo@ecubelabs.com>
2024-07-08 10:18:10 +0900

    FEAT: add postTitles

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-    let post = '강남 우동 맛집';
+    let [postTitles, b] = useState(['spring', 'summer', 'fall', 'winter']);
     return (
         <div className="App">
             <div className="black-nav">
-                <h4 style={{ color: 'red', fontSize: '16px' }}>블로그임</h4>
+                <h4 style={{ color: 'red', fontSize: '16px' }}>test blog </h4>
             </div>
-            <h4>{post}</h4>
+            {postTitles.map((postTitle) => (
+                <div className="list" key={postTitle}>
+                    <h4>{postTitle}</h4>
+                    <p>2월 17일 발행</p>
+                </div>
+            ))}
         </div>
     );
 }
