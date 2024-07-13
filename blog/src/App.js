4748f46d844cecde05ba96fa4eabaae35b4324d0
jovid <sh.jo@ecubelabs.com>
2024-07-14 01:05:17 +0900

    FEAT: make like button

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,20 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
 function App() {
     let [postTitles, b] = useState(['spring', 'summer', 'fall', 'winter']);
+    let [따봉, 따봉변경] = useState(0);
+
     return (
         <div className="App">
             <div className="black-nav">
                 <h4 style={{ color: 'red', fontSize: '16px' }}>test blog </h4>
             </div>
-            {postTitles.map((postTitle) => (
-                <div className="list" key={postTitle}>
-                    <h4>{postTitle}</h4>
-                    <p>2월 17일 발행</p>
-                </div>
-            ))}
+            <div className="list">
+                <h4>
+                    {postTitles[0]}
+                    <span onClick={() => 따봉변경(따봉 + 1)}>👍</span> {따봉}
+                </h4>
+                <p>2월 17일 발행</p>
+            </div>
+            <div className="list">
+                <h4>{postTitles[1]}</h4>
+                <p>2월 17일 발행</p>
+            </div>
+            <div className="list">
+                <h4>{postTitles[2]}</h4>
+                <p>2월 17일 발행</p>
+            </div>
+            <div className="list">
+                <h4>{postTitles[3]}</h4>
+                <p>2월 17일 발행</p>
+            </div>
         </div>
     );
 }
