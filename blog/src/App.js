7c24ca8547c9da4c9b42faa1341f1211ea79187a
jovid <sh.jo@ecubelabs.com>
2024-07-14 01:18:18 +0900

    FEAT: make array sort button

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-    let [postTitles, b] = useState(['spring', 'summer', 'fall', 'winter']);
+    let [글제목, 글제목변경] = useState(['spring', 'summer', 'fall', 'winter']);
     let [따봉, 따봉변경] = useState(0);
 
     return (
@@ -10,23 +10,41 @@ function App() {
             <div className="black-nav">
                 <h4 style={{ color: 'red', fontSize: '16px' }}>test blog </h4>
             </div>
+            <button
+                onClick={() => {
+                    let copy = [...글제목];
+                    copy.sort();
+                    글제목변경(copy);
+                }}
+            >
+                가나다순 정렬
+            </button>
+            <button
+                onClick={() => {
+                    let copy = [...글제목];
+                    copy[0] = '봄';
+                    글제목변경(copy);
+                }}
+            >
+                버튼
+            </button>
             <div className="list">
                 <h4>
-                    {postTitles[0]}
+                    {글제목[0]}
                     <span onClick={() => 따봉변경(따봉 + 1)}>👍</span> {따봉}
                 </h4>
                 <p>2월 17일 발행</p>
             </div>
             <div className="list">
-                <h4>{postTitles[1]}</h4>
+                <h4>{글제목[1]}</h4>
                 <p>2월 17일 발행</p>
             </div>
             <div className="list">
-                <h4>{postTitles[2]}</h4>
+                <h4>{글제목[2]}</h4>
                 <p>2월 17일 발행</p>
             </div>
             <div className="list">
-                <h4>{postTitles[3]}</h4>
+                <h4>{글제목[3]}</h4>
                 <p>2월 17일 발행</p>
             </div>
         </div>
