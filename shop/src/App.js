9f28836342d875acab965303f9ee6c23d6d19fab
jovid <sh.jo@ecubelabs.com>
2024-07-14 17:08:31 +0900

    FEAT: add shoes component

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,7 +1,11 @@
+import { useState } from 'react';
 import './App.css';
 import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
+import data from './data';
 
 function App() {
+    let [shoes] = useState(data);
+
     return (
         <div className="App">
             <Navbar bg="light" data-bs-theme="light">
@@ -17,25 +21,23 @@ function App() {
             <div>
                 <Container>
                     <Row>
-                        <Col sm={4}>
-                            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
-                            <h4>상품명</h4>
-                            <p>상품설명</p>
-                        </Col>
-                        <Col sm={4}>
-                            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
-                            <h4>상품명</h4>
-                            <p>상품설명</p>
-                        </Col>
-                        <Col sm={4}>
-                            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
-                            <h4>상품명</h4>
-                            <p>상품설명</p>
-                        </Col>
+                        {shoes.map((a, i) => {
+                            return <Card shoes={shoes[i]} i={i}></Card>;
+                        })}
                     </Row>
                 </Container>
             </div>
         </div>
+    );
+}
+
+function Card(props) {
+    return (
+        <Col sm={4}>
+            <img src={'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg'} width="80%" />
+            <h4>{props.shoes.title}</h4>
+            <p>{props.shoes.price}</p>
+        </Col>
     );
 }
 
