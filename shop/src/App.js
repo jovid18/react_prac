82dbcf175354a577d878b55d5c8912779c0ddf91
jovid <sh.jo@ecubelabs.com>
2024-07-14 17:33:53 +0900

    FEAT: add event page

--- shop/src/App.js
+++ shop/src/App.js
@@ -2,9 +2,11 @@ import { useState } from 'react';
 import './App.css';
 import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
 import data from './data';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
+import Detail from './routes/Details';
 function App() {
     let [shoes] = useState(data);
+    let navigate = useNavigate();
 
     return (
         <div className="App">
@@ -12,14 +14,23 @@ function App() {
                 <Container>
                     <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Cart</Nav.Link>
+                        <Nav.Link
+                            onClick={() => {
+                                navigate('/');
+                            }}
+                        >
+                            Home
+                        </Nav.Link>
+                        <Nav.Link
+                            onClick={() => {
+                                navigate('/detail');
+                            }}
+                        >
+                            Detail
+                        </Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
-            <Link to="/">홈페이지</Link>
-            <Link to="/detail">상세페이지</Link>
-
             <Routes>
                 <Route
                     path="/"
@@ -38,12 +49,31 @@ function App() {
                         </>
                     }
                 />
-                <Route path="/detail" element={<div>상세페이지임</div>} />
+                <Route path="/detail" element={<Detail />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/event" element={<EventPage />}>
+                    <Route path="one" element={<p>첫 주문시 양배추즙 서비스</p>}></Route>
+                    <Route path="two" element={<p>생일기념 쿠폰받기</p>}></Route>
+                </Route>
             </Routes>
         </div>
     );
 }
-
+function About() {
+    return (
+        <div>
+            <h4>회사 정보</h4>
+        </div>
+    );
+}
+function EventPage() {
+    return (
+        <div>
+            <h4>오늘의 이벤트</h4>
+            <Outlet></Outlet>
+        </div>
+    );
+}
 function Card(props) {
     return (
         <Col sm={4}>
