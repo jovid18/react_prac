9a60e7bd5dd4b446f2819932d6b61bf696f16e61
jovid <sh.jo@ecubelabs.com>
2024-07-14 18:25:20 +0900

    FEAT: add alert input

--- shop/src/routes/Details.js
+++ shop/src/routes/Details.js
@@ -1,21 +1,29 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 function Detail(props) {
-    useEffect(() => {
-        console.log('안녕');
-    });
     let { id } = useParams();
     let 찾은상품 = props.shoes.find(function (x) {
         return x.id == id;
     });
+    let [count, setCount] = useState(0);
+    let [alert, setAlert] = useState(true);
+    useEffect(() => {
+        let a = setTimeout(() => {
+            setAlert(false);
+        }, 2000);
+        return () => {
+            clearTimeout(a);
+        };
+    }, []);
     return (
         <div className="container">
-            <div className="alert alert-warning">2초이내 구매시 할인</div>
+            {alert == true ? <div className="alert alert-warning">2초이내 구매시 할인</div> : null}
             <div className="row">
                 <div className="col-md-6">
                     <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
                 </div>
+                <Input/>
                 <div className="col-md-6">
                     <h4 className="pt-5">{찾은상품.title}</h4>
                     <p>{찾은상품.content}</p>
@@ -24,6 +32,22 @@ function Detail(props) {
                 </div>
             </div>
         </div>
+    );
+}
+function Input() {
+    let [num, setNum] = useState('');
+    useEffect(() => {
+        if (isNaN(num) == true) {
+            alert('그러지마세요');
+        }
+    }, [num]);
+
+    return (
+        <input
+            onChange={(e) => {
+                setNum(e.target.value);
+            }}
+        />
     );
 }
 
